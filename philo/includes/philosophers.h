b087a032db0f651a8ed8c973c954fce1cb0ae013
mendes-jv <mendes.silva.jv@gmail.com>
2024-03-28 22:25:26 -0300

    Add Makefile, main and header files for the 'philo' project
    Implemented the foundations for the 'philo' project by adding a Makefile, a main.c file in the sources directory, and a philosophers.h header file. The Makefile includes various rules such as all, clean, fclean, re, and compilation rules, and it incorporates color indicators for successful or unsuccessful operations.

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 11:18:02 by jovicto2          #+#    #+#             */
+/*   Updated: 2024/03/28 11:18:04 by jovicto2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_PHILOSOPHERS_H
+# define PHILOSOPHERS_PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif //PHILOSOPHERS_PHILOSOPHERS_H
